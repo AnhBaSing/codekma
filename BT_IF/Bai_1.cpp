7512884c1e99a9a041a50de1eadc661902c9c933
AnhBaSing <bakumetsukma2429@gmail.com>
2025-03-18 17:38:33 +0700

    fixed bugs because forgot to save

--- BT_IF/Bai_1.cpp
+++ BT_IF/Bai_1.cpp
@@ -2,9 +2,10 @@
 #include <math.h>
 
 int main(){
-    int a, b, c, x1, x2, delta;
+    int a, b, c, delta;
+    double x1, x2;
     printf("Nhap 3 so a, b, c :");
-    scanf("%d %d %d", &a, &b, &c);;
+    scanf("%d, %d, %d", &a, &b, &c);;
     if (a == 0) {
         printf("Day khong phai phuong trinh bac hai.\n");
         return 0;
@@ -13,10 +14,10 @@ int main(){
         if (delta > 0){
             x1 = (-b + sqrt(delta))/(2*a);
             x2 = (-b - sqrt(delta))/(2*a);
-            printf("Phuong trinh co 2 nghiem phan biet: x1 = %d, x2 = %d", x1, x2 );
+            printf("Phuong trinh co 2 nghiem phan biet: x1 = %.2lf, x2 = %.2lf", x1, x2 );
         }   else if (delta == 0) {
             x1 = -b/(2*a);
-            printf("Phuong trinh co 2 nghiem kep: x1 = %d", x1);
+            printf("Phuong trinh co 2 nghiem kep: x1 = %.2lf", x1);
         }
             else if (delta < 0){
                 printf("Phuong trinh vo nghiem");
