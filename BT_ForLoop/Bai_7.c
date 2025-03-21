c2c0aabbfbadfb49d4dc9eaef39ef77894bcac8b
AnhBaSing <bakumetsukma2429@gmail.com>
2025-03-22 00:33:10 +0700

    full exercises completed for now, revise later

---
+++ BT_ForLoop/Bai_7.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    int n;
+    double i, sum = 0;
+    printf("Nhap so n: ");
+    scanf("%d", &n);
+    for (i = 1; i <= n; i++){
+        sum += 1/i;
+    }
+    printf("Tong S la %.2lf", sum);
+}
