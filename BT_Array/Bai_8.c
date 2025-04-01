2d3d54809d6ec3a4176ee958b811ddeea57d9b06
AnhBaSing <bakumetsukma2429@gmail.com>
2025-04-01 09:46:08 +0700

    updated

--- BT_Array/Bai_8.c
+++ BT_Array/Bai_8.c
@@ -23,10 +23,10 @@ int main(){
     for (int i = 0; i < n2; i++){
         c[i+n1] = b[i];
     }
-    // Ascending order sort
+    // Bubble Sort
     for (int i = 0;i<n3;i++){
         for (int j = i+1;j<n3;j++){
-            if (c[i] > c[j]){
+            if (c[i] > c[j]) {
                 temp = c[i];
                 c[i] = c[j];
                 c[j] = temp;
